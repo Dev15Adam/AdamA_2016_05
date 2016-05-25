a4c17c5506b9d29820a51e5aabd9a867bf14470b
Adam <adam.andersson.h@gmail.com>
2016-05-25 11:46:31 +0200

    Add project files.

---
+++ AdamA_2016_05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamA_2016_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
